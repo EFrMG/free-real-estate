3467b459559ba998c35661bfc7dc7b3e70acfa87
EFrMG <204592755+EFrMG@users.noreply.github.com>
2026-04-29 05:52:07 -0300

    simple home setup

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -5,15 +5,17 @@ export function meta({}: Route.MetaArgs) {
     { title: "Free Real State" },
     {
       name: "description",
-      content: "The place where your future place is found.",
+      content:
+        "Real estate company: The place where your future place is found.",
     },
   ];
 }
 
 export default function Home() {
   return (
-    <main>
+    <main className="grid grid-cols-[60%_40%] max-w-7xl w-full min-h-screen mx-auto">
       <div>HELLO!</div>
+      <div className="sm:bg-amber-100">HELLO!</div>
     </main>
   );
 }
